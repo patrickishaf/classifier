d1dcf3616ddf4ff3b2527649f8825b13b49a4b77
oscarnovember <ndukwe.peter@roqqu.com>
2026-04-23 06:18:13 +0100

    automatically run seed on app startup

--- src/db.ts
+++ src/db.ts
@@ -1,10 +1,28 @@
 import knex from 'knex';
 import dbConfig from '../knexfile';
 import { attachPaginate } from 'knex-paginate';
+import { join } from 'node:path';
+import { v7 as uuid } from 'uuid';
+import { readFileSync } from 'fs';
 
 const db = knex(dbConfig);
 attachPaginate();
 
 const getDB = () => db;
+
+export const runSeed = async (DB: knex.Knex) => {
+  await DB('profiles').del();
+
+  const json = JSON.parse(readFileSync(join(process.cwd(), 'src', 'db/seed_profiles.json'), 'utf-8'));
+
+  const profiles = json.profiles.map(d => ({
+    ...d,
+    id:uuid(),
+  }));
+
+  await DB.transaction(async (trx) => {
+    await trx.batchInsert('profiles', profiles, 100);
+  });
+}
 
 export default getDB;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import dotenv from 'dotenv';
 import app from './app';
 import { handleException, handleRejection } from './util';
+import getDB, { runSeed } from './db';
 
 dotenv.config();
 
 async function main() {
+  const db = getDB()
+  await runSeed(db);
   const port = process.env.PORT ?? '8080';
   app.listen(port, () => console.log('server listening on port ' + port));
   process.on('uncaughtException', handleException).on('unhandledRejection', handleRejection);
